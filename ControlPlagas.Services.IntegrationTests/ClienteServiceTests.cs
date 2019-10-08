0683e84dab77e52435fc61dece961de7fe3d296b
florin141 <29834996+florin141@users.noreply.github.com>
2019-10-09 00:06:10 +0200

    Setup integration tests project.

---
+++ ControlPlagas.Services.IntegrationTests/ClienteServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using ControlPlagas.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ControlPlagas.Services.IntegrationTests
+{
+    public class ClienteServiceTests : IntegrationTestsBase
+    {
+        [Fact]
+        public void Can_add_new_client_to_db()
+        {
+            var cliente = new Cliente
+            {
+                NombreCompleto = "Florin Ciobanu",
+                CorreoElectronico = "cflorin@hiberus.com",
+                Telefono = "643447860",
+                Direccion = "Zaragoza, Spain",
+                CodicoPostal = "50009"
+            };
+
+            ClienteService.Insert(cliente);
+
+            var fromDb = ClienteService.GetById(1);
+
+            Assert.NotNull(fromDb);
+        }
+    }
+}
---
+++ ControlPlagas.Services.IntegrationTests/IntegrationTestsBase.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using ControlPlagas.Core.Data;
+using ControlPlagas.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlPlagas.Services.IntegrationTests
+{
+    public class IntegrationTestsBase
+    {
+        private IContainer _container;
+
+        protected IContainer AutofacContainer
+        {
+            get
+            {
+                if (_container == null)
+                {
+                    var builder = new ContainerBuilder();
+
+                    builder.Register<DbContext>(ctx => new ControlPlagasObjectContext()).SingleInstance();
+
+                    builder.RegisterGeneric(typeof(ControlPlagasRepository<>)).As(typeof(IRepository<>)).SingleInstance();
+
+                    builder.RegisterType<ClienteService>().As<IClienteService>().InstancePerDependency();
+                    builder.RegisterType<FacturaService>().As<IFacturaService>().InstancePerDependency();
+                    builder.RegisterType<PlagaService>().As<IPlagaService>().InstancePerDependency();
+                    builder.RegisterType<TrabajadorService>().As<ITrabajadorService>().InstancePerDependency();
+                    builder.RegisterType<RecursoService>().As<IRecursoService>().InstancePerDependency();
+                    builder.RegisterType<ServicioService>().As<IServicioService>().InstancePerDependency();
+
+                    _container = builder.Build();
+                }
+
+                return _container;
+            }
+        }
+
+        protected IClienteService ClienteService
+        {
+            get
+            {
+                return AutofacContainer.Resolve<IClienteService>();
+            }
+        }
+
+        protected IFacturaService FacturaService
+        {
+            get
+            {
+                return AutofacContainer.Resolve<IFacturaService>();
+            }
+        }
+    }
+}
